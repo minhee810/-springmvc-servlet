d6039dedfca2647dc48939cf953d2a6286a3617b
minhee <minhee810@naver.com>
2024-01-11 23:11:00 +0900

    [add] 회원 정보 MVC 패턴

---
+++ src/main/java/com/hello/servlet/web/servletmvc/MvcMemberFormServlet.java
@@ -0,0 +1,26 @@
+package com.hello.servlet.web.servletmvc;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.io.IOException;
+
+@WebServlet(name = "mvcMemberFormServlet", urlPatterns = "/servlet-mvc/members/new-form")
+public class MvcMemberFormServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String viewPath = "/WEB-INF/views/new-form.jsp";
+
+        // controller 에서 view 로 이동할 때 사용되는 객체
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+
+        // Servlet 에서 JSP 를 호출
+        dispatcher.forward(request, response);
+    }
+}
